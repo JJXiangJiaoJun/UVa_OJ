74070f924fb33a559bbd3b15c5b00ff7f9d4b6ea
JJXiangJiaoJun <591495195@qq.com>
2018-10-20 23:23:24 +0800

    以后算法没有明显错误，考虑整数类型是否溢出，考虑long long

---
+++ UVa/UVa/ch8高效算法设计/UVa1614奇怪的股市.cpp
@@ -0,0 +1,46 @@
+#include <cstdio>
+#include <cstring>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+const int maxn = 1000000 + 1000;
+int A[maxn];
+int sum;
+int n;
+int sel[maxn];
+
+int main()
+{
+	freopen("C:\\Users\\lenovo\\Desktop\\test\\in.txt", "r", stdin);
+	freopen("C:\\Users\\lenovo\\Desktop\\test\\out.txt", "w", stdout);
+	while (scanf("%d", &n) == 1 && n)
+	{
+		for (int i = 0; i < n; i++)
+			scanf("%d", &A[i]);
+		sum = 0;
+		for (int i = 0; i < n; i++)
+			sum += A[i];
+		if (sum & 1) {
+			printf("No\n"); continue;
+		}
+		printf("Yes\n");
+		sum >>= 1;
+		for (int i = n - 1; i >= 0; i--)
+		{
+			if ( A[i]<= sum )
+			{
+				sum -= A[i];
+				sel[i] = 1;
+			}
+			else
+			{
+				sel[i] = -1;
+			}
+		}
+		for(int i=0;i<n-1;i++)
+			printf("%d ", sel[i]);
+		printf("%d\n", sel[n - 1]);
+	}
+	return 0;
+}
